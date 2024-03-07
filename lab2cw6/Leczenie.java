43a9c1cd52cf2b23428520ee3aa470b61c7ad38c
ogurecze <162505048+ogurecze@users.noreply.github.com>
2024-03-07 13:01:50 +0100

    Add files via upload

---
+++ lab2cw6/Leczenie.java
@@ -0,0 +1,6 @@
+public class Leczenie implements Pracowac{
+    @Override
+    public void pracuj() {
+        System.out.println("lecz sie");
+    }
+}
---
+++ lab2cw6/LiteraturaPopularnoNaukowa.java
@@ -0,0 +1,6 @@
+public class LiteraturaPopularnoNaukowa implements SpedzanieWolnegoCzasu{
+    @Override
+    public void spedzajWolnyCzas() {
+        System.out.printf("czytam czytam");
+    }
+}
